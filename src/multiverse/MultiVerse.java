de8651b45b9094d1f3f4dc0efc51ade1e5093be1
Itsvann878 <irvanadr8@gmail.com>
2024-12-19 10:58:42 +0700

    frame baru login dan home

---
+++ src/multiverse/MultiVerse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multiverse;
+
+/**
+ *
+ * @author itsva
+ */
+public class MultiVerse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginView lv = new LoginView();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
